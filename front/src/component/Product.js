f43604f94ea6c0afef47ad1ef7ed47606ae273ad
AdityaKakkar1402 <kakkar.adi14@gmail.com>
2022-07-25 16:45:29 +0530

    rating and product card is made

---
+++ front/src/component/Product.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import Rating from './Rating'
+
+const Product = ({product}) => {
+    return (
+    <Card className='my-3 p-3 rounded'>
+        <a href={`/product/${product._id}`}>
+           < Card.Img src={product.image} variant='top'/>
+        </a>
+        <Card.Body>
+            <a href={`/product/${product._id}`}id="textprod">
+               <Card.Title as='div'>
+                   <strong >
+                       {product.name}</strong></Card.Title> 
+                <Card.Text as='div'>
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+                </Card.Text>
+                <Card.Text as='h3' id="textprod">Rs.{product.price}</Card.Text>
+            </a>
+        </Card.Body>
+    </Card>
+    )
+}
+
+export default Product
