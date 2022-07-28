39b7f4ebd9808922510e8f78187ea71babd3b6ff
AdityaKakkar1402 <kakkar.adi14@gmail.com>
2022-07-28 17:10:38 +0530

    models introduction

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import products from './data/products.js'
+import connectDB from './config/db.js'
+import colors from 'colors'
 import dotenv from 'dotenv'
 
 dotenv.config()
+
+connectDB()
 
 const app=express()
 
@@ -22,5 +26,5 @@ app.get('/api/products/:id',(req,res)=>{
 const PORT=process.env.PORT || 5000
 
 app.listen(PORT,
-    console.log(`server is running in ${process.env.NODE_DEV} on port ${PORT}`)
+    console.log(`server is running in ${process.env.NODE_DEV} on port ${PORT}`.yellow.bold)
     )
